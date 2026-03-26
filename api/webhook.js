1d46f170866e36ae85146b8d8a2cad55005041e9
shahabassmak-arch <shahabassmak@gmail.com>
2026-03-26 05:32:58 +0530

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -40,7 +40,7 @@ export default async function handler(req, res) {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        message: userMessage,
+        prompt: userMessage,
       }),
     });
 
